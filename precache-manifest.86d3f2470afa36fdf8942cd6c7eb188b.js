235c84259bc6f18e4628b705f828cc988389e3b7
Xotonori <xotonori29011991@gmail.com>
2020-05-08 17:53:37 +0300

    Updates

---
+++ precache-manifest.86d3f2470afa36fdf8942cd6c7eb188b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ee1d272addca20b2bb01003cca53b5",
+    "url": "/react-counter/index.html"
+  },
+  {
+    "revision": "d21b2fca862f273e7d9d",
+    "url": "/react-counter/static/css/main.85a3e1a6.chunk.css"
+  },
+  {
+    "revision": "e58675299f4344e13e75",
+    "url": "/react-counter/static/js/2.382a85c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-counter/static/js/2.382a85c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21b2fca862f273e7d9d",
+    "url": "/react-counter/static/js/main.5b34fc9b.chunk.js"
+  },
+  {
+    "revision": "1743d70fa5c1f533df9c",
+    "url": "/react-counter/static/js/runtime-main.9072487c.js"
+  }
+]);
